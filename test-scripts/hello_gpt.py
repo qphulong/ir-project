814ec36a80a55cfbb442c511cb40ab0d4b432100
qplong21 <147737947+qphulong@users.noreply.github.com>
2024-11-12 23:51:17 +0700

    init README

---
+++ test-scripts/hello_gpt.py
@@ -0,0 +1,22 @@
+import sys
+import os
+
+SYSTEM_PATH = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+sys.path.append(SYSTEM_PATH)
+
+from openai import OpenAI
+from backend import env
+client = OpenAI()
+
+completion = client.chat.completions.create(
+    model="gpt-4o-mini",
+    messages=[
+        {"role": "system", "content": "You are a helpful assistant."},
+        {
+            "role": "user",
+            "content": "hello chat gpt"
+        }
+    ]
+)
+
+print(completion.choices[0].message)
