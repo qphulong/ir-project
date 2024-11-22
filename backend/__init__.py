aee0dc0f98513e979b3d58e6919f182cc55a5684
qplong <qphulong@gmail.com>
2024-11-22 22:52:31 +0700

    add NomicEmbed

--- backend/__init__.py
+++ backend/__init__.py
@@ -2,3 +2,4 @@ from .env import *
 from .query_preprocessor import *
 from .semantic_chunker import SemanticChunker
 from .paragraph_semantic_splitter import *
+from .nomic_embed import NomicEmbed
---
+++ backend/nomic_embed.py
@@ -0,0 +1,72 @@
+from typing import Any, Coroutine, List
+from llama_index.core.base.embeddings.base import Embedding
+from llama_index.core.embeddings import BaseEmbedding
+from sentence_transformers import SentenceTransformer
+from llama_index.core.bridge.pydantic import PrivateAttr
+
+from typing import List
+
+class NomicEmbed(BaseEmbedding):
+    """
+    Class to embed text to embeddings.
+    This class inherited with llama_index BaseEmbedding to
+    work with this lib components. See 
+
+    Attribute:
+        model: SentenceTransformer object with BaseEmbedding interface
+        precision: data type of output, for vector quantization
+        convert_to_numpy: Whether convert embeddings to np array or not.
+
+    Methods:
+        Well, the function names are self-explained. :)
+    """
+    _embed_model:SentenceTransformer = PrivateAttr()
+    _precision: str = PrivateAttr()
+    _convert_to_numpy: bool = PrivateAttr()
+    def __init__(self,model_path:str,**kwargs:Any):
+        super().__init__(**kwargs)
+        self._embed_model =  SentenceTransformer(
+            model_name_or_path=model_path,
+            trust_remote_code=True,
+            local_files_only=True
+        )
+        self._precision = 'float32'
+        self._convert_to_numpy = True
+
+    def _get_text_embedding(self, text:str):
+        embeddings = self._embed_model.encode(
+            sentences=text, 
+            prompt='search_document',
+            precision=self._precision,
+            convert_to_numpy=self._convert_to_numpy
+        )
+        return embeddings
+
+    async def _aget_text_embedding(self, text:str):
+        embeddings = await self._embed_model.encode(
+            sentences=text, 
+            prompt='search_document',
+            precision=self._precision,
+            convert_to_numpy=self._convert_to_numpy
+        )
+        return embeddings
+    
+    def _get_query_embedding(self, query: str):
+        embeddings = self._embed_model.encode(
+            sentences=query, 
+            prompt="search_query",
+            precision=self._precision,
+            convert_to_numpy=self._convert_to_numpy
+        )
+        return embeddings
+    
+    async def _aget_query_embedding(self, query: str):
+        embeddings = await self._embed_model.encode(
+            sentences=query, 
+            prompt="search_query",
+            precision=self._precision,
+            convert_to_numpy=self._convert_to_numpy
+        )
+        return embeddings
+    
+    
