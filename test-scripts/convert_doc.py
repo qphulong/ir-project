61a6ad4147e3bf8c8d6618396e149b4187806eb4
Duc7111 <phantrungduc2003@gmail.com>
2024-12-27 19:35:11 +0700

    test commit

--- test-scripts/convert_doc.py
+++ test-scripts/convert_doc.py
@@ -13,11 +13,21 @@ d2d = D2D()
 docx_data = d2d.convert_to_dict(docx_path)
 pdf_data = d2d.convert_to_dict(pdf_path)
 
+key = list(docx_data["content"].keys())[0]
+print(docx_data["content"][key]["embedding"].__class__)
+key = list(pdf_data["content"].keys())[0]
+print(docx_data["content"][key]["embedding"].__class__)
+
 d2d.save_to_disk("docx.json", docx_data)
 d2d.save_to_disk("pdf.json", pdf_data)
 
 docx_data = d2d.load_from_disk("docx.json")
 pdf_data = d2d.load_from_disk("pdf.json")
+
+key = list(docx_data["content"].keys())[0]
+print(docx_data["content"][key]["embedding"].__class__)
+key = list(pdf_data["content"].keys())[0]
+print(docx_data["content"][key]["embedding"].__class__)
 
 d2d.save_to_disk("docx2.json", docx_data)
 d2d.save_to_disk("pdf2.json", pdf_data)
